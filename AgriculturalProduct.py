c88046d7da58748ee7013150511c408630fae3ca
SartoriGuilherme <sartori.guilherme1@gmail.com>
2024-06-16 19:46:46 -0300

    .

--- AgriculturalProduct.py
+++ AgriculturalProduct.py
@@ -1,6 +1,1 @@
 from Product import Product
-
-class AgriculturalProduct(Product):
-
-    def __init__(self, name: str, price: float, quantity: float, additionalTransport: float):
-        super().__init__(name, price, quantity, additionalTransport)
